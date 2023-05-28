79ff2317f8535f855b43041add10476977291a4d
adrian <adrian.lpes@gmail.com>
2023-05-28 08:45:23 +0200

    feat: working on

--- src/common/providers/email/index.ts
+++ src/common/providers/email/index.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { BaseProvider } from '../base';
 import { TEmailConfig } from './interfaces';
+import * as nodemailer from 'nodemailer';
 
 const EMAIL = 'EMAIL' as const;
 
@@ -12,7 +13,16 @@ class EmailProvider extends BaseProvider {
     super();
     this.config = config;
   }
-  init() {
+  async init() {
+    const testAccount = await nodemailer.createTestAccount();
+    const transporter = nodemailer.createTransport({
+      ...testAccount.smtp,
+      auth: {
+        user: testAccount.user,
+        pass: testAccount.pass,
+      },
+    });
+    console.log({ transporter });
     this.updateStatus({
       status: 'ok',
       message: 'Email service running correctly',
